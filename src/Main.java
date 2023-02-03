855f1d538700983a4440952a9f9ce2986b4b234e
CintrianWarrior <velegoj2@yandex.ru>
2023-02-03 19:16:45 +0200

    Homework 1. Task 3.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Car firstCar = new Car("Lada", "Granta", 1.7, "Желтый", 2015, "Россия");
-        Car secondCar = new Car("Audi", "A8 50 L TDI quattro", 3.0, "Черный", 2020, "Германия");
-        Car thirdCar = new Car("BMW", "Z8", 3.0, "Черный", 2021, "Германия");
-        Car fourthCar = new Car("Kia", "Sportage 4", 2.4, "Красный", 2018, "Южная Корея");
-        Car fifthCar = new Car("Hyundai", "Avante", 1.6, "Оранжевый", 2016, "Южная Корея");
+        Car firstCar = new Car("  ", "Granta", 1.7, "Желтый", 2015, "Россия");
+        Car secondCar = new Car("Audi", "null", 3.0, "Черный", 2020, "Германия");
+        Car thirdCar = new Car("BMW", "Z8", 3.0, "Черный", 2021, "");
+        Car fourthCar = new Car("Kia", "Sportage 4", 0, "Красный", 2018, "Южная Корея");
+        Car fifthCar = new Car("Hyundai", "Avante", 1.6, "Оранжевый", -2012, "Южная Корея");
         System.out.println(firstCar);
         System.out.println(secondCar);
         System.out.println(thirdCar);
